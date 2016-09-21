96e8e0278f2d2a9eb5feb66eac72897d7682eb4a
Brad Chao <brad@brad.tw>
2016-09-21 09:31:13 +0800

    v1

---
+++ app/src/main/java/brad/tw/mysqlitetest/MyDBHelper.java
@@ -0,0 +1,27 @@
+package brad.tw.mysqlitetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by user on 2016/9/21.
+ */
+public class MyDBHelper extends SQLiteOpenHelper {
+    public MyDBHelper(Context context,
+                      String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+}
