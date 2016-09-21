b50e777a460b6b8078380156181cc9d5f432c676
Brad Chao <brad@brad.tw>
2016-09-21 10:20:35 +0800

    v3

--- app/src/main/java/brad/tw/mysqlitetest/MainActivity.java
+++ app/src/main/java/brad/tw/mysqlitetest/MainActivity.java
@@ -1,20 +1,49 @@
 package brad.tw.mysqlitetest;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MyDBHelper dbhelper;
     private SQLiteDatabase db;
+
+    private TextView mesg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mesg = (TextView)findViewById(R.id.mesg);
+
         dbhelper = new MyDBHelper(this,"brad", null, 1);
         db = dbhelper.getReadableDatabase();
 
     }
+    public void insert(View v){
+
+    }
+    public void delete(View v){
+
+    }
+    public void update(View v){
+
+    }
+    public void query(View v){
+        mesg.setText("");
+        Cursor c = db.query("cust",null,null,null,null,null,null);
+        while (c.moveToNext()){
+            String id = c.getString(c.getColumnIndex("id"));
+            String cname = c.getString(c.getColumnIndex("cname"));
+            String tel = c.getString(c.getColumnIndex("tel"));
+            String birthday = c.getString(c.getColumnIndex("birthday"));
+            mesg.append(id+":"+cname+":"+tel+":"+birthday+"\n");
+        }
+    }
+
 }
